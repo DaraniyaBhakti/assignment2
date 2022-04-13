862020df6cd1da2017a4ea5b61917b4003b311c8
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-04-13 19:34:06 +0530

    drop-down-added

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native';
 import RadioGroup from 'react-native-radio-buttons-group';
 import Checkbox from 'expo-checkbox';
 import DatePicker from 'react-native-datepicker'
+import DropdownComponent from './components/DropdownComponent';
 
 const radioButtonData = [
     { id:1, label:"Male", value:"Male"},
@@ -21,6 +21,7 @@ const FormScreen = () =>{
         
     }
     return(
+        <ScrollView>
         <View style={styles.container}>
             <TextInput style={styles.input} placeholder='Enter name' />
 
@@ -28,11 +29,39 @@ const FormScreen = () =>{
 
             <RadioGroup
                 containerStyle={styles.radioButton}
-                
                 radioButtons={radioButton}
                 onPress={onPressRadioButton}
                 layout='row'
             />
+
+            <View style={styles.datePicker}>
+                <DatePicker 
+                    style={{width:'100%'}}
+                    date={birthDate}
+                    mode="date"
+                    placeholder='Select birth date'
+                    format='DD-MM-YYYY'
+                    minDate={'01-01-1990'}
+                    maxDate={'31-12-2050'}
+                    confirmBtnText="Select"
+                    cancelBtnText='Cancel'
+                    customStyles={{
+                        dateIcon: {
+                            display: 'none',
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0,
+                        },
+                        dateInput: {
+                            marginLeft: 36,
+                        },
+                    }}
+                    onDateChange={(birthDate) => setBirthDate(birthDate)}
+                />
+            </View>
+
+            <DropdownComponent/>
 
             <View style={styles.checkBoxView}>
                 <View style={styles.checkBoxItemView}>
@@ -65,37 +94,12 @@ const FormScreen = () =>{
                     <Text  style={styles.checkBoxLabel}>Hindi</Text>
                 </View>
             </View>
-            <View style={styles.datePicker}>
-                <DatePicker 
-                    style={{width:'100%'}}
-                    date={birthDate}
-                    mode="date"
-                    placeholder='Select birth date'
-                    format='DD-MM-YYYY'
-                    minDate={'01-01-1990'}
-                    maxDate={'31-12-2050'}
-                    confirmBtnText="Select"
-                    cancelBtnText='Cancel'
-                    customStyles={{
-                        dateIcon: {
-                            display: 'none',
-                            position: 'absolute',
-                            left: 0,
-                            top: 4,
-                            marginLeft: 0,
-                        },
-                        dateInput: {
-                            marginLeft: 36,
-                        },
-                    }}
-                    onDateChange={(birthDate) => setBirthDate(birthDate)}
-                />
-            </View>
 
             <View style={styles.button}>
                 <Button  title='Submit' onPress={ () => alert("Done")}/>
             </View>
         </View>
+        </ScrollView>
     )
 }
 
