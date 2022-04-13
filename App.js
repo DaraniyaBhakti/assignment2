3b9fd3bcc88dbfb1ce3ee0b9e07e85d74fe2cb7a
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-04-13 11:23:23 +0530

    changed-heading-style

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'stretch',
     justifyContent: 'center',
-    marginTop:50
+    marginTop:20
   },
   item:{
     borderWidth:2,
